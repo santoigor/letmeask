3c7ffcba617395c7a71717cd7462ec70e7494c6e
Igor <igor.asantosss@outlook.com>
2021-07-15 20:41:55 -0300

    variáveis de ambiente foram inseridas

--- src/_services/firebase.ts
+++ src/_services/firebase.ts
@@ -1,16 +1,15 @@
 import firebase from 'firebase/app';
-
-import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDTbrvlkKVyNpKMyZbsP1RVOCQWvpQhcT0",
-    authDomain: "letmeask-2c367.firebaseapp.com",
-    databaseURL: "https://letmeask-2c367-default-rtdb.firebaseio.com",
-    projectId: "letmeask-2c367",
-    storageBucket: "letmeask-2c367.appspot.com",
-    messagingSenderId: "485195543932",
-    appId: "1:485195543932:web:93a6934fc7a968a0dba0f9"
+    apiKey: process.env.REACT_APP_API_KEY ,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN  ,
+    databaseURL: process.env.REACT_APP_DATABASE_URL  ,
+    projectId: process.env.REACT_APP_PROJECT_ID  ,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET  ,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID  ,
+    appId: process.env.REACT_APP_APP_ID  
 };
 
 firebase.initializeApp(firebaseConfig);
